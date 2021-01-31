540900303bd84dfa5136ec16273ea73bee9d7aa0
hulichao <jsu_hlc@163.com>
2021-01-31 23:40:13 +0800

    add

--- src/test/scala/t1.scala
+++ src/test/scala/t1.scala
@@ -1,10 +1,17 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.rdd.RDD
+
 object t1 {
   def main(args: Array[String]): Unit = {
-    val a: Double = 8.48
-    val b: Double = 121.88
-    val c: Double = a + b
-//    println(c)
+    val conf = new SparkConf().setAppName("FileDStream").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val rdd1: RDD[Int] = sc.makeRDD(1 to 10)
+    val rdd2: RDD[(Int, Int)] = sc.makeRDD((1 to 10).toList.zipWithIndex)
 
+    val result1 = rdd1.countByValue() //可以
+    val result2 = rdd1.countByKey() //语法错误
 
+    val result3 = rdd2.countByValue() //可以
+    val result4 = rdd2.countByKey() //可以
   }
 }
